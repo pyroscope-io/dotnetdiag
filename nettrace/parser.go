97243b32de142af43f91a998bc015fa8773fadc1
Anton Kolesnikov <anton.e.kolesnikov@gmail.com>
2021-05-17 16:07:50 +0200

    Improve UTF16 decoding performance

--- nettrace/parser.go
+++ nettrace/parser.go
@@ -19,6 +19,12 @@ func (p *Parser) Err() error {
 	return nil
 }
 
+func (p *Parser) Skip(n int) {
+	if n > 0 {
+		p.Next(n)
+	}
+}
+
 func (p *Parser) Read(v interface{}) {
 	if err := binary.Read(p.Buffer, binary.LittleEndian, v); err != nil {
 		p.errs = append(p.errs, err)
@@ -33,18 +39,34 @@ func (p *Parser) Uvarint() uint64 {
 	return n
 }
 
+// UTF16NTS returns UTF8 string read from 2-bytes UTF16 null terminated string.
+// Bytes are speculatively interpreted as 2-byte ASCII chars, and decoding
+// takes place only if a code unit is not representable in ASCII.
 func (p *Parser) UTF16NTS() string {
-	s := make([]uint16, 0, 256)
-	var c uint16
-	for {
-		if p.errs != nil {
-			return ""
-		}
-		p.Read(&c)
-		if c == 0x0 {
+	b := p.Bytes()
+	s := make([]byte, 0, 64) // The capacity has been chosen empirically.
+	for i := 0; i < len(b)-1; i += 2 {
+		if b[i] == 0x0 {
+			p.Skip(i + 2)
 			break
 		}
-		s = append(s, c)
+		if b[i+1] != 0x0 {
+			return p.decodeUTF16NTS()
+		}
+		s = append(s, b[i])
+	}
+	return string(s)
+}
+
+func (p *Parser) decodeUTF16NTS() string {
+	b := p.Bytes()
+	s := make([]uint16, 0, 64)
+	for i := 0; i < len(b)-1; i += 2 {
+		if b[i] == 0x0 {
+			p.Skip(i + 2)
+			break
+		}
+		s = append(s, binary.LittleEndian.Uint16(b[i:(i+2)]))
 	}
 	return string(utf16.Decode(s))
 }
